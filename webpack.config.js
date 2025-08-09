6980ba9251db4d0840a90a91d454ad9456d5c724
lxuanyang <liang_xuanyang@163.com>
2025-08-10 03:09:27 +0800

    push

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,66 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { VueLoaderPlugin } = require('vue-loader');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: "vue-loader",
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html'
+      template: "./public/index.html",
+      filename: "index.html",
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'public/favicon.svg',
-          to: 'favicon.svg'
-        }
-      ]
-    })
+          from: "public/favicon.svg",
+          to: "favicon.svg",
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      "@": path.resolve(__dirname, "src"),
+      vue$: "vue/dist/vue.esm.js",
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ["*", ".js", ".vue", ".json"],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
     port: 8080,
     hot: true,
-    open: true
-  }
+    open: true,
+  },
 };
